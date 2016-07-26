edf9220ecbe4538a6e713ff648a066d3feee6ed9
Maurits <maurits.rijk@gmail.com>
2016-07-26 12:36:46 +0200

    Fix typo

--- index.js
+++ index.js
@@ -10,19 +10,28 @@ const sergeants = [
     'Maurits',
     'Diego',
     'Tobias',
-    'Denny',
+    'Danny',
     'Chris O',
     'Robert'
 ];
 
+const schedule = [];
+
 app.set('port', process.env.PORT || 5000);
 
+// Return list of all sergeants
 app.get('/sergeant/list', (req, res) => {
     res.send({names: sergeants});
 });
 
+// Return sergeant of the day
 app.get('/sergeant/today', (req, res) => {
     res.send({name: randomSergeant()});
+});
+
+// return schedule for given sergeant
+app.get('/schedule/:name', (req, res) => {
+    res.send(req.name);
 });
 
 app.listen(app.get('port'), () => {
