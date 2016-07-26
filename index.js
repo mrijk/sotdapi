58d1914d1b2d145ca9a0c90f83183ae921bc496d
Maurits <maurits.rijk@gmail.com>
2016-07-26 11:07:14 +0200

    Different port

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 
 const express = require('express')
 const app = express()
- 
+
+app.set('port', process.env.PORT || 5000);
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
-app.listen(8080)
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
