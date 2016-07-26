d7714ae7ce98f920ee65b55f28aefede0880c0b4
Maurits <maurits.rijk@gmail.com>
2016-07-26 11:36:51 +0200

    Get random sergeant

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 'use strict';
 
+const _ = require('lodash');
 const express = require('express')
+
 const app = express()
+
+const sergeants = [
+    'Olaf',
+    'Maurits',
+    'Diego',
+    'Tobias',
+    'Denny',
+    'Chris O'
+];
 
 app.set('port', process.env.PORT || 5000);
 
-app.get('/sergeant/today', function (req, res) {
-    res.send({name: 'Olaf'});
-})
- 
-app.listen(app.get('port'), function() {
+app.get('/sergeant/today', (req, res) => {
+    res.send({name: randomSergeant()});
+});
+
+app.listen(app.get('port'), () => {
     console.log('Node app is running on port', app.get('port'));
 });
+
+function randomSergeant() {
+    return sergeants[_.random(sergeants.length - 1)];
+}
